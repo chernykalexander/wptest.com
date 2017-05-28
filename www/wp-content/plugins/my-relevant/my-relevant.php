2110991ebd71964967e2e35cf3fc21634f6143cf
chernykalexander <boxtab@bk.ru>
2017-05-28 11:02:22 +0300

    Add options for plugin for relevant

--- www/wp-content/plugins/my-relevant/my-relevant.php
+++ www/wp-content/plugins/my-relevant/my-relevant.php
@@ -27,6 +27,10 @@ function my_like_install() {
 	if ( version_compare( $wp_version, '3.5', '<' ) ) {
 		wp_die( 'Для данного плагина WordPress нужна версия 3.5 или выше.' );
 	};
+
+	// Если таких опций не существует то функция их создаст иначе обновит
+	update_option( 'my_relevant_options', 'random' );
+	update_option( 'my_relevant_count_options', '5' );
 };
 
 
@@ -36,6 +40,10 @@ register_deactivation_hook( __FILE__ , 'my_like_deactivate' );
 
 function my_like_deactivate() {
 	// делаем то, что нужно
+	
+	// Удаляем опции релевантного контента
+	delete_option( 'my_relevant_options' );
+	delete_option( 'my_relevant_count_options' );
 };
 
 
@@ -48,7 +56,7 @@ function my_relevant_create_submenu() {
 	// добавление подпункта меню в меню Настройки WordPress.
 	add_options_page( 
 		// название страницы
-		'Страница настроек плагина my-like', 
+		'Страница настроек плагина my-relevant',
 		// отображаемое название подпункта
 		'Меню my-relevant',
 		// уровень доступа, чтобы меню могли видеть только администраторы
@@ -60,7 +68,7 @@ function my_relevant_create_submenu() {
 	);
 
 	// вызываем функцию для регистрации настроек
-	add_action( 'admin_init', 'my_relevant_register_settings' );
+	// add_action( 'admin_init', 'my_relevant_register_settings' );
 };
 
 
@@ -76,8 +84,60 @@ function my_relevant_register_settings() {
 	// Это обязательное поле должно быть групповым именем, идентифицирующим все параметры в наборе. 
 	// Второй параметр — действительное имя параметра, которое должно быть уникальным.
 	// Третий параметр — функция обратного вызова для очистки значений параметров.
-	register_setting( 'my_relevant_group', 'my_relevant_options' );
+	// register_setting( 'my_relevant_group', 'my_relevant_options' );
 
 };
+
+
+
+function my_relevant_page() {
+?>
+	<div class="wrap">
+	<h2>Настройка плагина my-relevant</h2>
+	<form method="post" action="options.php">
+
+		<?php 
+			$my_relevant_options = get_option( 'my_relevant_options' ); 
+			$my_relevant_count_options = get_option( 'my_relevant_count_options' ); 
+		?>
+
+		<table class="form-table">
+			
+			<tr valign="top">
+			<th scope="row">Тип релевантного контента:</th>
+			<td>
+				<p>
+				<input type="radio" name="my_relevant_options" value="random" <?php if ($my_relevant_options == 'random') echo "checked"; ?> >
+				Случайные записи из первой рубрики данной записи
+				</p>
+				<p>
+				<input type="radio" name="my_relevant_options" value="author" <?php if ($my_relevant_options == 'author') echo "checked"; ?> >
+				Отображение дополнительных записей того же автора
+				</p>
+			</td>
+			</tr>
+
+			<tr valign="top">
+			<th scope="row">Количество релевантного контента:</th>
+			<td>
+				<p>
+				<input type="number" size="5" name="num" min="0" max="30" value="<?php echo $my_relevant_count_options; ?>">
+				Введите число от 0 до 30.
+				</p>
+			</td>
+			</tr>
+
+		</table>
+		
+		<p class="submit">
+			<input type="submit" class="button-primary" value="Сохранить изменения" />
+		</p>
+
+	</form>
+	</div>
+<?php
+} 
+?>
+
 
 
